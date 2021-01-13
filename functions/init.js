03c656775b55e054a369d2f4171b0bcd43e4ec51
octoxalis <octoxalis@gmail.com>
2021-01-13 15:39:28 +0100

    init

--- functions/init.js
+++ functions/init.js
@@ -1,7 +1,12 @@
+const SHARP_o = require( 'sharp' )
+
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
+  //const subject = event.queryStringParameters.name || 'World'
+  const sharp_o = SHARP_o
+  const status_s =
+   `Sharp status: ${sharp_o ? 'OK 😀' : 'FAIL 😡'}`
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`
+    body: `${status_s}`
   }
 }
